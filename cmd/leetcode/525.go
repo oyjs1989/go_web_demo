31046448cfcd7ed531a698d9cd6cafcf1097cb23
jason <348568175@qq.com>
2022-12-28 22:19:05 +0800

    add leetcode

---
+++ cmd/leetcode/525.go
@@ -0,0 +1,36 @@
+package main
+
+
+// 给定一个二进制数组 nums , 找到含有相同数量的 0 和 1 的最长连续子数组，并返回该子数组的长度。
+
+
+
+func findMaxLength(nums []int) (maxLength int) {
+    mp := map[int]int{0: -1}
+    counter := 0
+    for i, num := range nums {
+        if num == 1 {
+            counter++
+        } else {
+            counter--
+        }
+        if prevIndex, has := mp[counter]; has {
+            maxLength = max(maxLength, i-prevIndex)
+        } else {
+            mp[counter] = i
+        }
+    }
+    return
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
+func main() {
+	println(findMaxLength([]int{0, 1}))
+}
